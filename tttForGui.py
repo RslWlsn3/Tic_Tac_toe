282b2776c673e0eaafd7ad0d553de5263066c849
Connor Mote <connor27mote@gmail.com>
2019-01-11 14:57:55 -0700

    Added functoinal single player mode

--- tttForGui.py
+++ tttForGui.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 class Board:
     """Sets game board up. Also can edit and then display board"""
@@ -21,12 +22,13 @@ class Gameflow:
         self.turn = 0
         self.game_board = Board()  
         self.game_over = False
+        
 
-    def player_turn(self, x_move, y_move):
+    def player_turn(self, x_move, y_move, game_mode):
         """deside who's turn it is, ask for their move, check that it is valid"""                
         valide_move = self.validate_move(x_move,y_move)
         if valide_move and not self.game_over:
-            if (self.turn % 2) == 0:
+            if (self.turn % 2) == 0 or game_mode == '1':
                 self.game_board.edit_board(x_move, y_move, self.player_one)     
             else:
                 self.game_board.edit_board(x_move, y_move, self.player_two)            
@@ -49,65 +51,65 @@ class Gameflow:
     
     def check_rows(self):
         for row in self.game_board.board:
-            x_counter = 0
-            y_counter = 0
+            X_counter = 0
+            O_counter = 0
             for space in row:
                 if space.title() == 'X':
-                    x_counter +=1
+                    X_counter +=1
                 elif space.title() == 'O':
-                    y_counter +=1
-            if x_counter == 3:
+                    O_counter +=1
+            if X_counter == 3:
                 return True
-            elif y_counter == 3:                
+            elif O_counter == 3:                
                 return True
         return False
     
     def check_collums(self):
         for num in range(self.game_board.board_size):
-            x_counter = 0
-            y_counter = 0
+            X_counter = 0
+            O_counter = 0
             for collum in self.game_board.board:
                 if collum[num].title() == 'X':
-                    x_counter +=1
+                    X_counter +=1
                 elif collum[num].title() == 'O':
-                    y_counter +=1
-            if x_counter == 3:                
+                    O_counter +=1
+            if X_counter == 3:                
                 return True
-            elif y_counter == 3:                
+            elif O_counter == 3:                
                 return True
         return False
 
     def check_upRight_diagonal(self):
-        x_counter = 0
-        y_counter = 0        
+        X_counter = 0
+        O_counter = 0        
         for i in range(self.game_board.board_size):
             if self.game_board.board[i][self.game_board.board_size - 1 -i].title() == 'X':
-                x_counter +=1
+                X_counter +=1
             elif self.game_board.board[i][self.game_board.board_size -1 -i].title() == 'O':
-                y_counter +=1            
-        if x_counter == 3:            
+                O_counter +=1            
+        if X_counter == 3:            
             return True
-        elif y_counter == 3:            
+        elif O_counter == 3:            
             return True
         else:
             return False        
     
     def check_downLeft_diagonal(self):
-        x_counter = 0
-        y_counter = 0
+        X_counter = 0
+        O_counter = 0
         for i in range(self.game_board.board_size):            
             if self.game_board.board[i][i].title() == 'X':
-                    x_counter +=1
+                    X_counter +=1
             elif self.game_board.board[i][i].title() == 'O':
-                    y_counter +=1
-        if x_counter == 3:            
+                    O_counter +=1
+        if X_counter == 3:            
             return True
-        elif y_counter == 3:            
+        elif O_counter == 3:            
             return True
         else:
             return False
     
-    def check_catGame(self):
+    def check_catGame(self):    #could just check for turn == 9
         _count = 0
         for row in range(self.game_board.board_size):
             for collum in range(self.game_board.board_size):
@@ -125,8 +127,153 @@ class Gameflow:
         else:
             return False    
     
+#probably don't need new class
+class OnePlayerMode(Gameflow):
+        
+    def computer_play(self):
+        self.potential_com_move = []
+        move = self.com_check_rows()
+        if move: #checking rows
+            move.append(self.check_for_win()) 
+            return move
+        move = self.com_check_collums()
+        if move: #checking collums
+            move.append(self.check_for_win()) 
+            return move
+        move = self.com_check_downLeft_diagonal()
+        if move: #checking diagonal
+            move.append(self.check_for_win()) 
+            return move
+        move = self.com_check_upRight_diagonal()
+        if move: #checking other diagonal
+            move.append(self.check_for_win()) 
+            return move
+        elif self.potential_com_move:
+            self.game_board.edit_board(self.potential_com_move[0], self.potential_com_move[1], self.player_two)
+            return self.potential_com_move      
+        else:
+            move = self.Random_play()
+            Gameover = self.check_for_win()
+            move.append(Gameover)
+            return move
+
+    def com_check_for_space(self, potential_move, O_counter, X_counter):
+        """finds the blank space location which the computer may want play"""
+        if O_counter==2 and X_counter != 1:
+                self.game_board.edit_board(potential_move[0], potential_move[1], self.player_two)
+                potential_move.append(True) #make this move now
+                return potential_move
+        elif X_counter==2 and O_counter != 1:
+            #self.game_board.edit_board(potential_move[0], potential_move[1], self.player_two)
+            potential_move.append(False) #wait to make this move untill we see if there are any other win opertunities                
+            return potential_move
+        else:
+            return False
+
+    def com_check_rows(self):
+        for x, row in enumerate(self.game_board.board):
+            X_counter = 0
+            O_counter = 0            
+            potential_move = []
+            com_move = []           #I can make this simplier, should redue some logic----------------------------------
+            for y, space in enumerate(row):
+                if space.title() == 'X':
+                    X_counter +=1
+                elif space.title() == 'O':
+                    O_counter +=1
+                else:
+                    potential_move = [x,y]
+            if X_counter == 2 or O_counter == 2:           
+                com_move = self.com_check_for_space(potential_move, O_counter, X_counter) 
+            if com_move:
+                if com_move[2] == True:
+                    return com_move #win game if possible
+                else:
+                    self.potential_com_move = com_move #store block to play if there are no other win opurtunities
+        return False
+    
+    def com_check_collums(self):
+        for y in range(self.game_board.board_size):
+            X_counter = 0
+            O_counter = 0
+            potential_move = []
+            com_move = []
+            for x, collum in enumerate(self.game_board.board):
+                if collum[y].title() == 'X':
+                    X_counter +=1
+                elif collum[y].title() == 'O':
+                    O_counter +=1
+                else:
+                    potential_move = [x,y]
+            if X_counter ==2 or O_counter == 2:
+                com_move = self.com_check_for_space(potential_move, O_counter, X_counter) 
+            if com_move:
+                if com_move[2] == True:
+                    return com_move
+                else:
+                    self.potential_com_move = com_move
+        return False
+    
+    def com_check_upRight_diagonal(self):
+        X_counter = 0
+        O_counter = 0
+        potential_move = []
+        com_move = []        
+        for x in range(self.game_board.board_size):
+            y = self.game_board.board_size - 1 - x
+            if self.game_board.board[x][y].title() == 'X':
+                X_counter +=1
+            elif self.game_board.board[x][y].title() == 'O':
+                O_counter +=1 
+            else: 
+                potential_move = [x,y]          
+        if X_counter ==2 or O_counter == 2:
+                com_move = self.com_check_for_space(potential_move, O_counter, X_counter) 
+        if com_move:
+            if com_move[2] == True:
+                    return com_move
+            else:
+                self.potential_com_move = com_move
+        return False    
+    
+    def com_check_downLeft_diagonal(self):
+        X_counter = 0
+        O_counter = 0
+        potential_move = []
+        com_move = []
+        for x in range(self.game_board.board_size):
+            y = x            
+            if self.game_board.board[x][y].title() == 'X':
+                X_counter +=1
+            elif self.game_board.board[x][y].title() == 'O':
+                O_counter +=1
+            else:
+                potential_move = [x,y]
+        if X_counter ==2 or O_counter == 2:
+                com_move = self.com_check_for_space(potential_move, O_counter, X_counter) 
+        if com_move:
+            if com_move[2] == True:
+                    return com_move
+            else:
+                self.potential_com_move = com_move
+        return False 
+    
+    def Random_play(self):
+        x = random.randrange(3)    
+        y = random.randrange(3)    
+        while not self.validate_move(x, y): #recurtoin wasn't working here
+            x = random.randrange(3)    
+            y = random.randrange(3)    
+        else:
+            self.game_board.edit_board(x, y, self.player_two)            
+            xyList = [x,y]
+            return xyList
+    
     
 
 
+# go = OnePlayerMode()
+# go.Random_play()
 
+#will prioitize a row block over a collumn win
 
--- tttGui.py
+++ tttGui.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 import tttForGui
 import os
 import time
+from functools import partial
 
 class TTTGui:
 
@@ -14,79 +15,63 @@ class TTTGui:
         self.label_text = " Player 1's turn "
         self.label = Label(self.root, text= self.label_text)
         self.label.grid(row=0, columnspan = 3)
+        self.button_list = []
+        self.game_mode = input("1. Single player\n2.Two player")
         self.gui()
-        
+          
 
-    def go(self, x, y, btn_num):
-        turn_worked = self.game.player_turn(x, y)
+    def button_pressed(self, xyList, *nothing):
+        x = xyList[0]
+        y = xyList[1]
+        btn_num = (x*3)+y
+        turn_worked = self.game.player_turn(x, y, self.game_mode)
         if self.turn % 2 == 0 and turn_worked:
-            btn_num["text"] = ' X '
+            btn = self.button_list[btn_num]
+            btn["text"] = ' X '
             self.label.config(text=" Player 2's turn ") 
-            self.turn +=1                           
+            self.turn +=1
+                                       
         elif turn_worked:
-            btn_num["text"] = ' O '
+            btn = self.button_list[btn_num]
+            btn["text"] = ' O '
             self.label.config(text=" Player 1's turn ")
             self.turn +=1
-        if turn_worked == 2:
-             self.label.config(text=" Game Over ")
-             self.gameOver = True             
-                     
+            
+        if turn_worked == 2: #2 means game over but this isn't very clear-----------
+            self.label.config(text=" Game Over ")
+            self.gameOver = True 
+            
+        elif self.game_mode == '1' and turn_worked:
+            comp_list =  self.game.computer_play()            
+            btn_num = (comp_list[0]*3)+comp_list[1]
+            btn = self.button_list[btn_num]
+            btn["text"] = ' O '
+            self.label.config(text=" Player 1's turn ")
+            self.turn +=1 
+            if comp_list[2] is True:
+                self.label.config(text=" Game Over ")
+                self.gameOver = True                      
 
     def gui(self):
         #b1
-        btn_text = " _ "
-        btn_hieght = 3
-        btn_width = 7
-        B1 = Button(self.root, text = btn_text, height = 3, width = btn_width)
-        command= lambda x: self.go(0, 0, B1)
-        B1.grid(row=1, column=0)
-        B1.bind("<Button-1>", command)
-        #b2
-        B2 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(0,1, B2)
-        B2.grid(row=1, column=1)
-        B2.bind("<Button-1>", command)
-        #b3
-        B3 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(0,2, B3)
-        B3.grid(row=1, column=2)
-        B3.bind("<Button-1>", command)
-        #b4
-        B4 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(1,0, B4)
-        B4.grid(row=2, column=0)
-        B4.bind("<Button-1>", command)
-        #b5
-        B5 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(1, 1, B5)
-        B5.grid(row=2, column=1)
-        B5.bind("<Button-1>", command)
-        #b6
-        B6 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(1, 2, B6)
-        B6.grid(row=2, column=2)
-        B6.bind("<Button-1>", command)
-        #b7
-        B7 = Button(self.root, text=" _ ",height = 3, width = btn_width)
-        command= lambda x: self.go(2, 0, B7)
-        B7.grid(row=3, column=0)
-        B7.bind("<Button-1>", command)
-        #b8
-        B8 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(2, 1, B8)
-        B8.grid(row=3, column=1)
-        B8.bind("<Button-1>", command)
-        #b9
-        B9 = Button(self.root, text=" _ ", height = 3, width = btn_width)
-        command= lambda x: self.go(2, 2, B9)
-        B9.grid(row=3, column=2)
-        B9.bind("<Button-1>", command)
+        self.btn_text = " _ "
+        self.btn_hieght = 3
+        self.btn_width = 7        
+        
+        for row in range(3):
+            for column in range(3):
+                btn = Button(self.root, text = self.btn_text, height = self.btn_hieght, width = self.btn_width)
+                xyList = [row, column]            
+                command = partial(self.button_pressed, xyList)
+                btn.grid(row=row+1, column=column)
+                btn.bind("<Button-1>", command)
+                self.button_list.append(btn)
 
         self.root.mainloop()
+    
 
-    def __del__(self):
-        print("deleted")
+    
 
-setup = tttForGui.Gameflow()
+setup = tttForGui.OnePlayerMode()
 play = TTTGui(setup)
 
